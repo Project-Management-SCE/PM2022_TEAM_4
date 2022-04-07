d76838799ee9a708bdb384e2d9a57ee9f6b3e3bf
LevAlhazov <60731755+LevAlhazov@users.noreply.github.com>
2022-04-07 15:42:42 +0300

    AUTH application fix.

--- NextDoor/Account/url.py
+++ NextDoor/Account/url.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.urls import path
-from . views import SignupPageView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('signup/', SignupPageView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
