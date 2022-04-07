b68dd50bd4ee9c312c0fd6498e1292da064fbb6e
LevAlhazov <60731755+LevAlhazov@users.noreply.github.com>
2022-04-07 15:36:17 +0300

    url fix.

--- NextDoor/Account/url.py
+++ NextDoor/Account/url.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from NextDoor.Account.migrations import views
+from . import views
 from django.urls import path
 from . views import SignupPageView
 from django.contrib.auth import views as auth_views
