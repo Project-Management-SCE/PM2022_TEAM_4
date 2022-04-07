cb98dae26c50d0f366a1cb681c7f6643b69160ee
ziv abutbul <73219144+ziv-abutbul@users.noreply.github.com>
2022-04-07 16:05:24 +0300

    create sign page front
    BSPM2022T4-6,7,8

--- NextDoor/Account/url.py
+++ NextDoor/Account/url.py
@@ -5,6 +5,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('signup/', SignupPageView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
--- NextDoor/Account/views.py
+++ NextDoor/Account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import Group
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views import generic
+from .form import CustomUserCreationForm
+from .models import CustomUser
+
+class SignupPageView(generic.CreateView):
+    pass
