814fe5a54e1f0cff8286f493ad09d5666ad66998
GalArzi <73073781+GalArzi@users.noreply.github.com>
2022-04-12 22:56:46 +0300

    Signup Tests - UnitTest
    4 tests:
      - Setup
      - Sign up temple
      - Sign up form
      - Sign up view

--- NextDoor/Account/tests.py
+++ NextDoor/Account/tests.py
@@ -35,3 +35,30 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
 
+
+class SignupTests(TestCase):  # new
+    username = 'BobTheTester'
+    email = 'newuser@email.com'
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'Account/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(
+            self.response, 'invalid Page.')
+
+    def test_signup_form(self):  # new
+        form = self.response.context.get('form')
+
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self): # new
+        view = resolve('/Account/signup/')
+        self.assertEqual(
+        view.func.__name__,
+        SignupPageView.as_view().__name__)
