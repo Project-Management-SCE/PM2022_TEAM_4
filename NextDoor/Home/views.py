594481354f47ef0eeb75107524d78b07a3481d0a
ziv abutbul <73219144+ziv-abutbul@users.noreply.github.com>
2022-04-22 15:08:18 +0300

    fix image on map

--- NextDoor/Home/views.py
+++ NextDoor/Home/views.py
@@ -13,11 +13,10 @@ def map(request):
     profile = UserProfile.objects.get(user=get_user)
     #add to map who is login
     m = folium.Map(location=[profile.latitude,profile.longitude],zoom_start=15)
-    ##print(profile.image)
-    ##icon = folium.features.CustomIcon(profile.image.url, icon_size=(28, 30))
-    #print(icon)
-    ##folium.Marker([profile.latitude, profile.longitude], tooltip="You here!", popup='כאן בהמשך תפתח בקשה', marker_icon=icon).add_to(m)
-    folium.Marker([profile.latitude, profile.longitude], tooltip="You here!", popup="<a href=https://http://127.0.0.1:8000>Help Me</a>",icon=folium.Icon(color='green')).add_to(m)
+    print(profile.image.url)
+    userIcon = 'http://127.0.0.1:8000' + profile.image.url
+    icon = folium.features.CustomIcon(userIcon, icon_size=(28, 30))
+    folium.Marker([profile.latitude, profile.longitude], tooltip="You here!", popup="<a href=https://http://127.0.0.1:8000>Help Me</a>",icon=icon).add_to(m)
     #make the raduis circle
     folium.Circle(location=[profile.latitude, profile.longitude], radius=100).add_to(m)
     #save Lat and lan who login
@@ -29,8 +28,10 @@ def map(request):
         if tempProfile:
             cleveland_oh = (tempProfile.latitude, tempProfile.longitude)
             #get the lan and lat from all user and add to map if under 0.1
-            if distance.distance(newport_ri, cleveland_oh).km < 0.1 and tempProfile != profile:
-                 folium.Marker([tempProfile.latitude, tempProfile.longitude], tooltip="click for help me "+user.username, popup='אני צריך מקדחה' ,icon=folium.Icon(color='red', icon='info-sign')).add_to(m)
+            if distance.distance(newport_ri, cleveland_oh).km <= 0.1 and tempProfile != profile:
+                otherusericon='http://127.0.0.1:8000' + tempProfile.image.url
+                icon = folium.features.CustomIcon(otherusericon, icon_size=(28, 30))
+                folium.Marker([tempProfile.latitude, tempProfile.longitude], tooltip="click for help me "+user.username, popup='אני צריך מקדחה' ,icon=icon).add_to(m)
     m = m._repr_html_()
     context = { 'm':m,'profile':profile}
     return render(request,'home/Map.html',context)
