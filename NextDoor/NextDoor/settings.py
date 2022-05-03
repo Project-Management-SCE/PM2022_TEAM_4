10941ada541df3e9f158fe09decfb571d4d6131d
ziv abutbul <73219144+ziv-abutbul@users.noreply.github.com>
2022-05-03 18:33:39 +0300

    update DB online

--- NextDoor/NextDoor/settings.py
+++ NextDoor/NextDoor/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'NextDoor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hfiwjxrm',
+        'USER': 'hfiwjxrm',
+        'PASSWORD': 'IHQ4iObnT85q-kEg7qdFciL4Qp9aEDlP',
+        'HOST': 'rogue.db.elephantsql.com',
+        'PORT': '',
     }
 }
 
