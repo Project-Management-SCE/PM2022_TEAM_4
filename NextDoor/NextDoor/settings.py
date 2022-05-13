a9904b9284570860be47c8b5938b52abfb5dcca8
LevAlhazov <60731755+LevAlhazov@users.noreply.github.com>
2022-05-13 11:16:01 +0300

    Update settings.py

--- NextDoor/NextDoor/settings.py
+++ NextDoor/NextDoor/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import sys
 from pathlib import Path
-import django_heroku
+import django-heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -160,4 +160,4 @@ GOOGLE_MAPS_API_KEY = 'AIzaSyD8UkSs_yEEw3SgfYVTe1Gkdxz2pnu-ju0'
 
 GRAPPELLI_ADMIN_TITLE = 'NextDoor - Administration Panel'
 
-django_heroku.settings(locals())
+django-heroku.settings(locals())
